348a2817e46d19f5e96aa07f960fcc13c821d669
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-05 11:54:12 -0300

    Update Clima.vue

--- src/components/Clima.vue
+++ src/components/Clima.vue
@@ -13,11 +13,14 @@
 
 <template>
     <div class="resultado">
-        <h2>Clima de: {{ clima.name }}</h2>
-        <p class="actual">{{ formatearTemperatura(clima.main.temp) }} &deg;C</p>
+        <h2>Clima de: {{ clima.name }} </h2>
+        
+        <p class="actual"> {{ formatearTemperatura(clima.main.temp) }} &deg;C</p>
+        
         <div class="temperaturas">
-            <p>Min: <span>{{ formatearTemperatura(clima.main.temp_min) }} &deg;C</span></p>
-            <p>Max: <span>{{ formatearTemperatura(clima.main.temp_max) }} &deg;C</span></p>
+            <p>Min: <span> {{ formatearTemperatura(clima.main.temp_min) }} &deg;C</span></p>
+            <p>Max: <span> {{ formatearTemperatura(clima.main.temp_max) }} &deg;C</span></p>
         </div>
+        
     </div>
 </template>
