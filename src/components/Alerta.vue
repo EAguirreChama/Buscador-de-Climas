0ed131688a19c5195aa98d57be5b2e7b8b889097
Ernesto Aguirre <125767165+EAguirreChama@users.noreply.github.com>
2023-09-20 11:25:18 -0300

    Update Alerta.vue

--- src/components/Alerta.vue
+++ src/components/Alerta.vue
@@ -5,13 +5,13 @@
 </template>
 
 <style>
-    .alerta {
-        text-align: center;
-        margin-bottom: 2rem;
-        color: var(--blanco);
-        text-transform: uppercase;
-        font-weight: 900;
-        font-size: 1.8rem;
-        padding: .5rem
-    }
+.alerta {
+    text-align: center;
+    margin-bottom: 2rem;
+    color: var(--blanco);
+    text-transform: uppercase;
+    font-weight: 900;
+    font-size: 1.8rem;
+    padding: .5rem
+}
 </style>
